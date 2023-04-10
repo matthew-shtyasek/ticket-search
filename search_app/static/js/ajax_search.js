140fe071c4ddd47bdcc1ab443b19725091dbc54c
Your Name <you@example.com>
2023-04-10 11:58:58 +0500

    написали ajax

---
+++ search_app/static/js/ajax_search.js
@@ -0,0 +1,24 @@
+function cleanList() {
+    $('ul.ajax-list')[0].remove('li');
+}
+
+(function loadScript() {
+    $(document).ready(function () {
+        $('.search-btn').click(function (e) {
+            e.preventDefault(); // отмена события по умолчанию, при нажатии на кнопку теперь ничего не происходит
+            cleanList(); // очистка списка, написали эту функцию выше
+
+            $.ajax({
+                url: 'search/tickets/',
+                data: $('form').serialize(),
+                success: function (data) {
+                    $('ul.ajax-list').append(data);
+                    $('.error-message').text('')
+                },
+                error: function () {
+                    $('.error-message').text('Некорректно заполнена форма');
+                }
+            });
+        });
+    });
+})();
