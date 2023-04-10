a3bb7f1c6cc31918cb526610bfcf03f0c84e166f
Your Name <you@example.com>
2023-04-10 11:03:44 +0500

    подключили jquery и root.html

--- course_example/settings.py
+++ course_example/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'course_example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
